433069f40604fd63f1d21b96838c6cd6b9e9b273
Tiago Santos <tiago.santos@cortex-intelligence.com>
2014-09-11 11:37:43 +0100

    Enables definition of various jetty instances.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,18 @@
-class jetty(
+define jetty (
   $version,
   $home        = '/opt',
   $manage_user = true,
   $user        = 'jetty',
   $group       = 'jetty',
+  $name        = 'jetty',
 ) {
 
   include wget
+  $pattern_jetty_home = regsubst('^cd "\$JETTY_HOME"', '/', '\\/', 'G', 'U')
+  $replacement_jetty_home = regsubst("cd \"${home}/jetty\"", '/', '\\/', 'G', 'U')
+  $pattern_jetty_pid = regsubst('^  JETTY_PID="\$JETTY_RUN/jetty.pid"', '/', '\\/', 'G', 'U')
+  $replacement_jetty_pid = regsubst("  JETTY_PID=\"\\\$JETTY_RUN/${name}.pid\"", '/', '\\/', 'G', 'U')
+  $file = "${home}/jetty-distribution-${version}/bin/jetty.sh"
 
   if $manage_user {
 
@@ -14,7 +20,7 @@ class jetty(
       managehome => true,
       system     => true,
       gid        => $group,
-      before     => Exec['jetty_untar'],
+      before     => Exec["jetty_untar_${name}"],
     })
 
     ensure_resource('group', $group, { 
@@ -22,35 +28,54 @@ class jetty(
     })
   }
 
-  wget::fetch { "jetty_download":
-    source      => "http://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/${version}/jetty-distribution-${version}.tar.gz",
-    destination => "/usr/local/src/jetty-distribution-${version}.tar.gz",
+  file { "/usr/local/src/${name}":
+    ensure => "directory",
   } ->
-  exec { "jetty_untar":
-    command => "tar xf /usr/local/src/jetty-distribution-${version}.tar.gz && chown -R ${user}:${group} ${home}/jetty-distribution-${version}",
+  file { "${home}":
+    ensure => "directory",
+  }->
+  wget::fetch { "jetty_download_${name}":
+    source      => "http://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/${version}/jetty-distribution-${version}.tar.gz",
+    destination => "/usr/local/src/${name}/jetty-distribution-${version}.tar.gz",
+  } ->
+  exec { "jetty_untar_${name}":
+    command => "tar xf /usr/local/src/${name}/jetty-distribution-${version}.tar.gz && chown -R ${user}:${group} ${home}/jetty-distribution-${version}",
     cwd     => $home,
     creates => "${home}/jetty-distribution-${version}",
     path    => ["/bin",],
-    notify  => Service["jetty"]
+    notify  => Service[$name]
   } ->
   
   file { "${home}/jetty":
     ensure => "${home}/jetty-distribution-${version}",
   } ->
 
-  file { "/var/log/jetty":
-    ensure => "${home}/jetty/logs",
+  file { "/var/log/${name}":
+    ensure => "${home}/${name}/logs",
   } ->
 
-  file { "/etc/default/jetty":
+  file { "/etc/default/${name}":
     content => template('jetty/default'),
   } ->
 
-  file { "/etc/init.d/jetty":
-    ensure => "${home}/jetty-distribution-${version}/bin/jetty.sh",
+  file { "${file}":
+    ensure => present,
+  } ->
+  exec { "replace JETTY_HOME for ${name} in jetty.sh":
+    command => "/usr/bin/perl -pi -e 's/${pattern_jetty_home}/${replacement_jetty_home}/' '${file}'",
+    onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_jetty_home}/ && ! /\\Q${replacement_jetty_home}\\E/; END { exit \$ret; }' '${file}'",
+    alias => "jetty_${name}",
+  } ->
+  exec { "replace JETTY_PID for ${name}.pid in jetty.sh":
+    command => "/usr/bin/perl -pi -e 's/${pattern_jetty_pid}/${replacement_jetty_pid}/' '${file}'",
+    onlyif => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_jetty_pid}/ && ! /\\Q${replacement_jetty_pid}\\E/; END { exit \$ret; }' '${file}'",
+    alias => "pid_${name}",
+  } ->
+  file { "/etc/init.d/${name}":
+    ensure => "${file}",
   } ~>
 
-  service { "jetty":
+  service { $name:
     enable     => true,
     ensure     => running,
     hasstatus  => false,
